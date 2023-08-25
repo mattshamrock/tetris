a38ef7b48609638684dcb318adab573800118c29
mattshamrock <97572720+mattshamrock@users.noreply.github.com>
2023-08-25 21:46:28 +0900

    Update artlib_search32_xsxs.py

--- artlib_search32_xsxs.py
+++ artlib_search32_xsxs.py
@@ -44,112 +44,111 @@ board,  board,  board,  board,  board,  board, board,  board,  board ]
 blockorder = [6,7,4,2,3,1,5]
 
 
-def dfs(currentboard,Block,status):
+def dfs(currentboard, num_move, status):
     global foundflag
-    if foundflag == True:
-            pass
+    num_move += 1
+    if num_move > maxmoves:
+        # final check of the board
+        # two x-coords must have the two shape indexes respectively. Others must be empty         
+        printflag1 = CheckTwoBlocks(currentboard = status[maxmoves])
+        # all rows apart from the bottom must be empty
+        printflag2 = CheckClearRows(currentboard = status[maxmoves])
+
+        if printflag1 == True and printflag2 == True:
+            print("found one")
+            foundflag = True
+            # return the procedure that allows the two blocks to remain.
+            return status[0]
     else:
-        block = Block
-        block += 1
-        if block > maxblock:
-            printflag1, printflag2 = True, True
-            
-            printflag1 = CheckTwoBlocks(currentboard = status[maxblock])
-            printflag2 = CheckClearRows(currentboard = status[maxblock])
+        # search all combinations of shape, rotate and x-coord to see if it fits
+        for shape in blockorder:
+            for rotate in range(len(refpoint_relative[shape])):
+                for Xgrid in range(Xrange[shape][rotate][0],Xrange[shape][rotate][1]):
+                    # search for appropriate y-coord of refpoint that settles the block
+                    # search from the top row and look for the first y-coord such that the grid to be placed is already filled.
+                    # the appropriate y-coord of the refpoint is the one above it
+                    for Ygrid in range(2,range_y+1):
+                        newboard = copy.deepcopy(currentboard)
+                        refpoint = [Xgrid,Ygrid]
+                        
+                        gridref = newboard[refpoint[1]][refpoint[0]] 
+                        grid0 = newboard[refpoint[1] + refpoint_relative[shape][rotate][0][1]][refpoint[0] +  refpoint_relative[shape][rotate][0][0]]
+                        grid1 = newboard[refpoint[1] + refpoint_relative[shape][rotate][1][1]][refpoint[0] +  refpoint_relative[shape][rotate][1][0]]
+                        grid2 = newboard[refpoint[1] + refpoint_relative[shape][rotate][2][1]][refpoint[0] +  refpoint_relative[shape][rotate][2][0]]
+                        if gridref == 0 and grid0 == 0 and grid1 == 0 and grid2 == 0:
+                            pass
+                        else:
+                            refpoint = [refpoint[0],refpoint[1]-1]
 
-            if printflag1 == True and printflag2 == True and foundflag == False:
-                print("found one")
-                path = '..\\tetris\\game_manager\\log_art.txt'
-                with open (path, mode ="a") as f:
-                    f.write("xs_" + str(x1)+str(shape1)+str(x2)+str(shape2)+ " = " + str(status[0])+ "\n")            
-                foundflag = True
-                """
-                for i in range(range_y+1):
-                    with open (path, mode ="a") as f:
-                        f.write(str(status[maxblock][i])+ "\n")
-                """
-        else:
-            for shape in blockorder:
-                for rotate in range(len(refpoint_relative[shape])):
-                    for Xgrid in range(Xrange[shape][rotate][0],Xrange[shape][rotate][1]):
-                        for Ygrid in range(2,range_y+1):
-                            newboard = copy.deepcopy(currentboard)
-                            refpoint = [Xgrid,Ygrid]
-                            
-                            gridref = newboard[refpoint[1]][refpoint[0]] 
-                            grid0 = newboard[refpoint[1] + refpoint_relative[shape][rotate][0][1]][refpoint[0] +  refpoint_relative[shape][rotate][0][0]]
-                            grid1 = newboard[refpoint[1] + refpoint_relative[shape][rotate][1][1]][refpoint[0] +  refpoint_relative[shape][rotate][1][0]]
-                            grid2 = newboard[refpoint[1] + refpoint_relative[shape][rotate][2][1]][refpoint[0] +  refpoint_relative[shape][rotate][2][0]]
-                            if gridref == 0 and grid0 == 0 and grid1 == 0 and grid2 == 0:
-                                pass
+                            # simulate the board after the block is dropped
+                            newboard[refpoint[1]][refpoint[0]] = shape
+                            for add_grid in range(3):
+                                newboard[refpoint[1] + refpoint_relative[shape][rotate][add_grid][1]][refpoint[0] +  refpoint_relative[shape][rotate][add_grid][0]] = shape
+
+                            # clear fulllines
+                            clearedboard = clearFullLines(currentboard = newboard)
+
+                            contflag1 = CheckBottomRow(currentboard = clearedboard)
+                            contflag2 = CheckTopRow(currentboard = clearedboard)
+                            contflag3 = CheckHoles(currentboard = clearedboard)
+
+                            if contflag1 == True and contflag2== True and contflag3 == True:
+                                status[0][num_move-1][0] = shape
+                                status[0][num_move-1][1] = rotate
+                                status[0][num_move-1][2] = Xgrid
+                                status[num_move] = clearedboard
+                                path = '..\\tetris\\game_manager\\log_art.txt'
+                                with open (path, mode ="a") as f:
+                                    f.write(str(status[0]) + "\n")
+
+                                ans = dfs(currentboard = status[num_move],num_move = num_move,status = status)
+                                if foundflag == True:
+                                    return ans
+                                status[0][num_move-1] = [0,0,0,1]
+                                    
                             else:
-                                refpoint = [refpoint[0],refpoint[1]-1]
-
-                                newboard[refpoint[1]][refpoint[0]] = shape
-                                for add_grid in range(3):
-                                    newboard[refpoint[1] + refpoint_relative[shape][rotate][add_grid][1]][refpoint[0] +  refpoint_relative[shape][rotate][add_grid][0]] = shape
-
-                                clearedboard = clearFullLines(currentboard = newboard)
-
-                                status[0][block-1][0] = shape
-                                status[0][block-1][1] = rotate
-                                status[0][block-1][2] = Xgrid
-                                status[block] = clearedboard
-
-                                contflag1,contflag2,contflag3 = True,True,True
-                                contflag1 = CheckBottomRow(currentboard = status[block])
-                                contflag2 = CheckTopRow(currentboard = status[block])
-                                contflag3 = CheckHoles(currentboard = status[block])
-
-                                if contflag1 == True and contflag2== True and contflag3 == True:
-                                    dfs(currentboard = status[block],Block = block,status = status)
-                                    status[0][block-1] = [0,0,0,1]
-                                        
-                                else:
-                                    break
                                 break
+                            break
 
 def CheckBottomRow(currentboard):
-    check = False
     BottomRow = currentboard[range_y - 1]
     i = 0
     for eachgrid in BottomRow:
         if eachgrid != 0:
             if (i == x1 and eachgrid == shape1) or (i == x2 and eachgrid == shape2):
-                check = True
+                pass
             else:
-                check = False
-                break
+                return False
         i += 1
-    return check
+    return True
 
 
 def CheckTwoBlocks(currentboard):
-    check = True
     board = currentboard
     Y = range_y -1 
     for X in range(10):
         TestShape = board[Y][X]
+        # first x-coord and shape1
         if X == x1:
             if TestShape != shape1:
-                check = False 
+                return False
+        # second x-coord and shape2
         elif X == x2:
             if TestShape != shape2:
-                check = False 
+                return False
+        # others must be empty
         else:
             if TestShape != 0:
-                check = False
-    return check
+                return False
+    return True
 
 def CheckTopRow(currentboard):
-    check = True
     for num in range(3):
         TopRow = currentboard[num]
         for eachgrid in TopRow:
             if eachgrid != 0:
-                check = False
-                break
-    return check
+                return False
+    return True
 
 def clearFullLines(currentboard):
     board = currentboard
@@ -163,7 +162,6 @@ def clearFullLines(currentboard):
     return board
 
 def CheckHoles(currentboard):
-    check = True
     board = currentboard
     for X in range(10):
         holecandidate = False
@@ -172,45 +170,44 @@ def CheckHoles(currentboard):
                 holecandidate = True
             else:
                 if holecandidate == True:
-                    check = False
-                    break
+                    return False
 
-    return check
+    return True
 
 def CheckClearRows(currentboard):
-    check = True
-    for num in range(6):
+    for num in range(range_y - 1):
         TopRow = currentboard[num]
         for eachgrid in TopRow:
             if eachgrid != 0:
-                check = False
-                break
-    return check
-
-def writeNone():
-    path = '..\\tetris\\game_manager\\log_art.txt'
-    with open (path, mode ="a") as f:
-        f.write("xs_" + str(x1)+str(shape1)+str(x2)+str(shape2)+ ":" + "False," + "\n")
+                return False
+    return True
 
 
 def search_artlib():
     global foundflag
     foundflag = False
+    num_move = 0
     default_status = copy.deepcopy(status)
-    print(x1,shape1,x2,shape2)
+    ans = dfs(currentboard = status[1], num_move = num_move, status = default_status)
+    if ans == None:
+        return False
+    return ans
 
-    ans = dfs(currentboard = status[1],Block = block, status = default_status)
-    if foundflag == False:
-        ans = writeNone()
+def write_in_txt(ans):
+    path = '..\\tetris\\game_manager\\log_art.txt'
+    with open (path, mode ="a") as f:
+        f.write('"xs_' + str(x1)+str(shape1)+str(x2)+str(shape2)+ '":' + str(ans)+ "," + "\n")
 
-
-
-
-maxblock = 8
-block = 0
+maxmoves = 8
+# for each combination of two x-coords and two shapes
 for x1 in range(10):
     for x2 in range(x1+1,10-x1+1):
         for shape1 in blockorder:
             for shape2 in blockorder:
+                print(x1,shape1,x2,shape2)
+                #  return the procedure that allows the two blocks to remain. Return False if not found.
                 ans = search_artlib()
+                # write the ans in txt
+                write = write_in_txt(ans = ans)
+
 print("All done")
